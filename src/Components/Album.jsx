ce8c59ecdcd371946c18608b85e94880e6b19579
s0583232209 <s0583232209@gmail.com>
2025-12-25 21:38:32 +0200

    I added the pictures to the data base and added some connections between the albums component and the photo new one,

--- src/Components/Album.jsx
+++ src/Components/Album.jsx
@@ -1,3 +1,19 @@
+import Photo from './Photo'
+import {useEffect} from 'react'
 export default function Album(props) {
-  return <h1>Albums one 2 three</h1>;
+  
+  return <><h1>Albums one 2 three</h1><Photo path='../src/assets/pictures/landscape1.jpg' />
+    <Photo path='../src/assets/pictures/landscape2.jpg' />
+    <Photo path='../src/assets/pictures/landscape3.jpg' />
+    <Photo path='../src/assets/pictures/landscape4.jpg' />
+    <Photo path='../src/assets/pictures/landscape5.jpg' />
+    <Photo path='../src/assets/pictures/landscape6.jpg' />
+    <Photo path='../src/assets/pictures/landscape7.jpg' />
+    <Photo path='../src/assets/pictures/landscape8.jpg' />
+    <Photo path='../src/assets/pictures/landscape9.jpg' />
+    <Photo path='../src/assets/pictures/landscape10.jpg' />
+    <Photo path='../src/assets/pictures/landscape11.jpg' />
+    <Photo path='../src/assets/pictures/landscape12.jpg' />
+    <Photo path='../src/assets/pictures/landscape13.jpg' />
+    </>
 }

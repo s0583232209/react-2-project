82616fb4600061fe9175f528a55d06b608cd7008
s0583232209 <s0583232209@gmail.com>
2025-12-29 17:26:24 +0200

    Many new exciting things in the albums!!

--- src/Components/AlbumLink.jsx
+++ src/Components/AlbumLink.jsx
@@ -1,15 +1,29 @@
-import { useState , useEffect } from "react";
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { NavLink, Outlet, useNavigate, useHref } from "react-router-dom";
 import Album from "./Album";
 export default function AlbumLink(props) {
   const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  const href = useHref();
+  useEffect(() => {
+    console.log(href);
+    
+    if (show) {
+        props.changeStateAlbumView(true)
+        navigate(`${href}/${props.id}`)};
+  }, [show]);
   return (
     <>
-      <button onClick={() => setShow(true)}>
+      <button
+        onClick={() => {
+         console.log('in on Click');
+         
+          setShow(true);
+           ;
+        }}
+      >
         Press to open album {props.id}
       </button>
-      {show ? <Album id={props.id} title={props.title}></Album> : null}
-      <br></br>
     </>
   );
 }
